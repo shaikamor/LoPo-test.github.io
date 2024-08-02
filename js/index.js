69b6b7aebb6718358ca850a7ebf6169eac768d77
shaikamor <shaikamor@gmail.com>
2024-08-03 02:52:23 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,14 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginMenuItem = document.getElementById('loginMenuItem');
     const logoutMenuItem = document.getElementById('logoutMenuItem');
     const logoutButton = document.getElementById('logoutButton');
+    const welcomeMessage = document.getElementById('welcomeMessage');
 
     if (isLoggedIn) {
         loginMenuItem.style.display = 'none';
         logoutMenuItem.style.display = 'block';
+        const userFullName = localStorage.getItem('userFullName');
+        welcomeMessage.innerHTML = `Welcome back, ${userFullName}!`;
     }
 
     logoutButton.addEventListener('click', () => {
         localStorage.removeItem('isLoggedIn');
+        localStorage.removeItem('userFullName');
         window.location.href = 'index.html'; // Redirect to home page after logout
     });
 });
